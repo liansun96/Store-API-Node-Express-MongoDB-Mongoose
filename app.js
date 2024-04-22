9290c78e1753a7af34e0a9615591b6a429d9407d
liansun96 <toetetaung3296@gmail.com>
2024-04-22 22:28:41 +0630

    setup schema models & automating adding data to database

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
-const productsRouter = require('./routes/products')
+const productsRouter = require("./routes/products");
 
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">Product Rote</a>');
 });
 
 app.use(express.json());
-app.use('/api/v1/products' , productsRouter)
+app.use("/api/v1/products", productsRouter);
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
