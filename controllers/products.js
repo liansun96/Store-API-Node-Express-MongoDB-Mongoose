9708a9457b05040427ee90fb5a0204de34809542
liansun96 <toetetaung3296@gmail.com>
2024-04-30 22:01:45 +0630

    update

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,14 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
-const getAllProducts = async (req, res) => {  
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({ featured: true });
+
   res.status(200).json({ msg: "products testing route" });
 };
 
 const getAllPouductsStatic = async (req, res) => {
-  const products = await Product.find({featured : true})
-  res.status(200).json({ products , nbHit : products.length});
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHit: products.length });
 };
 
 module.exports = { getAllProducts, getAllPouductsStatic };
