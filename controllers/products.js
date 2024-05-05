92b8cb7b7346f1a4ba9fcd4932ed209415d37252
liansun96 <toetetaung3296@gmail.com>
2024-05-05 23:29:22 +0630

    update

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({ featured: true });
+  // const products = await Product.find({ featured: true });
 
   res.status(200).json({ msg: "products testing route" });
 };
